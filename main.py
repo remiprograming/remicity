f54858217b7c79c68e37b697d22d6246503b289b
Kamil Dudek <kamil.dudek5.stud@pw.edu.pl>
2021-07-27 16:38:29 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+
+
+running = True
+playing = True
+
+
+
+pg.init()
+screen = pg.display.set_mode((600, 600))
+clock = pg.time.Clock()
+
+
+
+while running:
+
+    while playing:
+        #a
